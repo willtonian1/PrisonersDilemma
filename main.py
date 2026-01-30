97cf86fef2b43dd09d259b1678c07894b9323260
willtonian1 <williamstom2005@gmail.com>
2026-01-30 13:43:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+
+class match():
+    def __init__(self, p1, p2):
+        p1 = p1
+        p2 = p2
+    def play():
+        return 0
+    
+
 class tournament():
     def __init__(self):
         x = 0
