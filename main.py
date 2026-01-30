c88cb2f6a31f8f5f04d7641be6267d1829bcf5ba
willtonian1 <williamstom2005@gmail.com>
2026-01-30 13:57:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,23 @@ class titForTat():
     def __init__(self):
         self.name = "TitForTat"
     def decision():
+
+        # if opponent rejected
+            # then reject
+        # else
+            # then accept
+
+
         return 0
+
+class alwaysAccept():
+    def __init__(self):
+        self.name = "AlwaysAccept"
+    def decision():
+        return 1
+
+# 1 for accept 
+# 0 for reject
 
 class match():
     def __init__(self, p1, p2):
@@ -21,6 +37,7 @@ class tournament():
         for policy in self.policies:
             print(policy.name)
 
-a = titForTat()
-t = tournament([a])
+titfortat = titForTat()
+titfortat2 = alwaysAccept()
+t = tournament( [titfortat, titfortat2] )
 t.run()
